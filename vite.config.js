cedc5545323820e986f0c50e3bc5a2e17c02b247
Tim Shichalin <shichatim@gmail.com>
2024-11-11 22:42:55 -0800

    vite setup

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig} from 'vite';
+import dns from 'node:dns';
+
+dns.setDefaultResultOrder('verbatim');
+
+export default defineConfig({
+    server: {
+        port: 3000,
+    },
+    root: './frontend'
+});
