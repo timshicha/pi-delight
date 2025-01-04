63e8ebac0148fb1d33a16a74f00435426a5f0d49
VadimShicha <75353180+VadimShicha@users.noreply.github.com>
2025-01-03 23:30:34 -0800

    added shoot button and health bar

--- server/ShooterGame.js
+++ server/ShooterGame.js
@@ -3,14 +3,39 @@ export class ShooterGame {
 
         this.gameType = "Shooter Game";
 
-        this.players = {}; // username: {matches: 0, gender: 'boy'}
+        this.players = {}; // username: {position: {x: 0, y: 0}, gender: 'boy'}
         this.sendRefreshFunc = sendRefreshFunc;
+
+        // Add each player
+        for (let i = 0; i < players.length; i++) {
+            const username = players[i];
+            if(username in this.players) continue; // Don't add duplicates
+            this.players[username] = {
+                position: { x: 0, y: 0 },
+                gender: 'boy'
+            };
+        }
+
+        // constantly send a refresh to update players positions
+        setInterval(() => {
+            this.sendRefreshFunc();
+        }, 50);
 
         this.sendRefreshFunc();
     }
 
+    makeMove = (username, moveInfo) => {
+        // check to see which type of move was made
+        // if a player move was made, update the player position
+        if(moveInfo.moveType == "playerMove") {
+            this.players[username].position = moveInfo.newPosition;
+        }
+    }
+
     getVisibleState = () => {
-        return {};
+        return {
+            players: this.players
+        };
     }
 
     removePlayer = (username) => {
